aa1eec0ec07d7fcbce79f5247ebc5299223b6c4d
Dominik Grzedzielski <gie.dominik@gmail.com>
2021-04-29 16:31:51 +0200

    Add init config

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    semi: ['error', 'always'],
+    'prettier/prettier': ['error'],
+    'import/no-unresolved': 'off',
+    'import/absolute-first': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+      },
+    ],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  }
+}
